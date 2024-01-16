c9c10da47c7fc1edac45832b3f203a7a60bcdea8
saurbh balke <20bcs201@iiitdmj.ac.in>
2024-01-16 23:08:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {} from 'googlemaps';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'task';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('gmapContainer', { static: false }) gmap: ElementRef<any>;
+  map: google.maps.Map;
+  lat = 22.6709;
+  lng = 75.8275;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 15
+  };
+  
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    console.log(this.mapOptions);
+    this.map = new google.maps.Map(this.gmap.nativeElement, 
+    this.mapOptions);
+    const trafficLayer = new google.maps.TrafficLayer();
+
+    trafficLayer.setMap(this.map);
+    const marker = new google.maps.Marker({position: this.coordinates, map: this.map})
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule} from '@angular/google-maps'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
